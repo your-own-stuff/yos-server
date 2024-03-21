d69f15bced2c757c11d17b5b0404cc7465e6509a
Jan Ka <Jan-Ka@users.noreply.github.com>
2024-03-21 15:44:27 +0100

    test: add benchmarks for generate-index_test.go
    1.1 seconds for 10.000 files

--- controller/generate-index_test.go
+++ controller/generate-index_test.go
@@ -1,6 +1,9 @@
 package controller
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"testing"
 
 	"github.com/pocketbase/pocketbase/tests"
@@ -23,6 +26,80 @@ func TestTraversDirAndBuildIndex(t *testing.T) {
 	}
 }
 
+func setupBenchmarkingFolder(rootPath string, folders int, filesPer int) (string, error) {
+	benchPath, err := os.MkdirTemp(rootPath, "benchmark")
+	if err != nil {
+		return "", err
+	}
+
+	lastPath := benchPath
+
+	for i := 1; i <= folders; i++ {
+		for j := 1; j < filesPer; j++ {
+			_, err := os.CreateTemp(lastPath, fmt.Sprintf("bl%d", j))
+
+			if err != nil {
+				return "", err
+			}
+		}
+
+		lastPath, err = os.MkdirTemp(lastPath, fmt.Sprintf("bl%d", i))
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return benchPath, nil
+}
+
+func cleanupBenchmarkingFolder(path string) {
+	err := os.RemoveAll(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+var benchmarkTraversDirAndBuildIndexRuns = []struct {
+	folders  int
+	filesPer int
+}{
+	{folders: 1, filesPer: 10},
+	{folders: 5, filesPer: 20},
+	{folders: 5, filesPer: 200},
+	{folders: 20, filesPer: 500},
+	{folders: 50, filesPer: 200},
+	{folders: 2, filesPer: 20},
+}
+
 func BenchmarkTraversDirAndBuildIndex(b *testing.B) {
-	// TODO: Write benchmark
+	testApp, err := tests.NewTestApp(pbTestDataDir)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer testApp.Cleanup()
+
+	rootPath, err := os.MkdirTemp("", "benchmark")
+	if err != nil {
+		b.Fatal(err)
+	}
+	fmt.Printf("Benchmarking folder: %s\n", rootPath)
+	defer cleanupBenchmarkingFolder(rootPath)
+
+	for _, v := range benchmarkTraversDirAndBuildIndexRuns {
+		b.Run(fmt.Sprintf("Folders_%d_FilesPer_%d", v.folders, v.filesPer), func(rb *testing.B) {
+			path, err := setupBenchmarkingFolder(rootPath, v.folders, v.filesPer)
+			if err != nil {
+				rb.Fatal(err)
+			}
+
+			rb.ResetTimer()
+
+			err = traversDirAndBuildIndex(testApp.Dao(), path, "test")
+
+			if err != nil {
+				rb.Fatal(err)
+			}
+		})
+	}
+
 }
